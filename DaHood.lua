bd118e852813f70811485aaada32ba03918d4c3f
Chungdz09 <chungdz210809@gmail.com>
2025-05-06 12:34:18 +0700

    Create DaHood.lua

---
+++ DaHood.lua
@@ -0,0 +1,97 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Window = Rayfield:CreateWindow({
+    Name = "                                DaHood XD",
+    LoadingTitle = "WSP",
+    LoadingSubtitle = "Made by Chungdz credit to Friend",
+    ConfigurationSaving = {
+       Enabled = flase,
+       FolderName = nil, 
+       FileName = "Nigga 69"
+    },
+
+    KeySystem = true,
+    KeySettings = {
+       Title = "Island Tribes 🌴",
+       Subtitle = "Made by Chung credit #Chungdz",
+       Note = "this guy is gay gbaox_01",
+       FileName = "0", 
+       SaveKey = true,
+       GrabKeyFromSite = true,
+       Key = {"https://raw.githubusercontent.com/Chungdz09/Script-chung-dz/refs/heads/main/Key"}
+    }
+ })
+
+ local MainTab = Window:CreateTab("🏠 Main", nil) 
+ local MainSection = MainTab:CreateSection("Other Scripts")
+local Button = MainTab:CreateButton({
+   Name = "Ctrl + click TP",
+   Callback = function()
+    loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/Click%20Teleport.txt"))()  
+  end,
+})
+
+local Button = MainTab:CreateButton({
+   Name = "Infinite yield",
+   Callback = function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()  
+  end,
+})
+
+local Button = MainTab:CreateButton({
+   Name = "Dark Dex",
+   Callback = function()
+    loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/Dex%20Explorer.txt"))()
+  end,
+})
+
+local Button = MainTab:CreateButton({
+   Name = "ESP",
+   Callback = function()
+    loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/WRD%20ESP.txt"))()
+   end,
+})
+
+local Button = MainTab:CreateButton({
+   Name = "AimBot Press X On/Off",
+   Callback = function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Chungdz09/Script-chung-dz/refs/heads/main/KchungdzAimbot.lua"))()
+   end,
+})
+
+local MainSection = MainTab:CreateSection("Toggles")
+
+local Toggle = MainTab:CreateToggle({
+   Name = "Toggle Inf Jump",
+   CurrentValue = false,
+   Flag = "Toggle1", 
+   Callback = function(Value)
+   loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/Infinite%20Jump.txt"))()
+   end,
+})
+
+local MainSection = MainTab:CreateSection("Sliders")
+
+local Slider = MainTab:CreateSlider({
+   Name = "WalkSpeed Slider",
+   Range = {1, 350},
+   Increment = 1,
+   Suffix = "Speed",
+   CurrentValue = 16,
+   Flag = "sliderws", 
+   Callback = function(Value)
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = (Value)
+   end,
+})
+
+local Slider = MainTab:CreateSlider({
+   Name = "JumpPower Slider",
+   Range = {1, 350},
+   Increment = 1,
+   Suffix = "Speed",
+   CurrentValue = 16,
+   Flag = "sliderjp",
+   Callback = function(Value)
+        game.Players.LocalPlayer.Character.Humanoid.JumpPower = (Value)
+   end,
+})
+  
