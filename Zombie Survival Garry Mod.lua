147aa3beeb1099f127fdfb5a0064fbc33375de3f
Stallion2108 <chungdz210809@gmail.com>
2025-05-21 20:27:58 +0700

    Update Zombie Survival Garry Mod.lua

--- Zombie Survival Garry Mod.lua
+++ Zombie Survival Garry Mod.lua
@@ -1,6 +1,6 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
-    Name = "                                DaHood XD",
+    Name = "                              Zombie Survival Garry Mod XD",
     LoadingTitle = "WSP",
     LoadingSubtitle = "Made by Chungdz",
     ConfigurationSaving = {
