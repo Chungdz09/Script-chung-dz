d335e9ded9d6ccf9c1d14594c680fe5e6a8c8385
Stallion2108 <chungdz210809@gmail.com>
2025-05-24 14:44:08 +0700

    Create Arsenal.lua

---
+++ Arsenal.lua
@@ -0,0 +1,75 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Window = Rayfield:CreateWindow({
+    Name = "                                Arsenal",
+    LoadingTitle = "WSP",
+    LoadingSubtitle = "Made by Chungdz credit to Friend",
+    ConfigurationSaving = {
+       Enabled = flase,
+       FolderName = nil, 
+       FileName = "Nigga 69"
+    },
+
+    KeySystem = true,
+    KeySettings = {
+       Title = "Arsenal",
+       Subtitle = "Made by Chung credit #Chungdz",
+       Note = "this guy is gay gbaox_01",
+       FileName = "0", 
+       SaveKey = true,
+       GrabKeyFromSite = true,
+       Key = {"https://raw.githubusercontent.com/Chungdz09/Script-chung-dz/refs/heads/main/Key"}
+    }
+ })
+
+ local MainTab = Window:CreateTab("🏠 Main", nil) 
+ local MainSection = MainTab:CreateSection("Other Scripts")
+
+local Button = MainTab:CreateButton({
+   Name = "Ctrl + click TP",
+   Callback = function()
+    loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/Click%20Teleport.txt"))()  
+  end,
+})
+
+local Button = MainTab:CreateButton({
+   Name = "Infinite yield",
+   Callback = function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()  
+  end,
+})
+
+local Button = MainTab:CreateButton({
+   Name = "Dark Dex",
+   Callback = function()
+    loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/Dex%20Explorer.txt"))()
+  end,
+})
+
+local Button = MainTab:CreateButton({
+   Name = "Draw Bones",
+   Callback = function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Stallion2108/Script-chung-dz/refs/heads/main/Draw%20Bones.lua"))()
+  end,
+})
+
+local Button = MainTab:CreateButton({
+   Name = "ESP",
+   Callback = function()
+    loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/WRD%20ESP.txt"))()
+   end,
+})
+
+local Button = MainTab:CreateButton({
+   Name = "Draw Reddot",
+   Callback = function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Stallion2108/Script-chung-dz/refs/heads/main/Draw%20Reddot%20on%20head"))()
+   end,
+})
+
+local Button = MainTab:CreateButton({
+   Name = "Aimbot FOV",
+   Callback = function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Stallion2108/Script-chung-dz/refs/heads/main/Arsenal%20Aimbot.txt"))()
+   end,
+})
+
