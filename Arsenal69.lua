518ed3b56586a6dca2f34312cc5196f78d94304c
Stallion2108 <chungdz210809@gmail.com>
2025-06-06 15:13:06 +0700

    Update Arsenal69.lua

--- Arsenal69.lua
+++ Arsenal69.lua
@@ -134,7 +134,7 @@ local Button = MainTab:CreateButton({
 local Button = MainTab:CreateButton({
    Name = "ESP",
    Callback = function()
-    loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/WRD%20ESP.txt"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Stallion2108/Script-chung-dz/refs/heads/main/ArsenalESP.lua"))()
    end,
 })
 
