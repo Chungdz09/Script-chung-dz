5d18ce6559b184a8b037f3b40f009db718048fe0
Stallion2108 <chungdz210809@gmail.com>
2025-06-06 14:08:52 +0700

    Update PVPSCRIPT.lua

--- PVPSCRIPT.lua
+++ PVPSCRIPT.lua
@@ -121,32 +121,6 @@ local Toggle = MainTab:CreateToggle({
    end,
 })
 
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
-local Window = Rayfield:CreateWindow({
-    Name = "                                Island Tribes 🌴",
-    LoadingTitle = "WSP",
-    LoadingSubtitle = "Made by Chungdz credit to Friend",
-    ConfigurationSaving = {
-       Enabled = flase,
-       FolderName = nil, 
-       FileName = "Nigga 69"
-    },
-
-    KeySystem = true,
-    KeySettings = {
-       Title = "Island Tribes 🌴",
-       Subtitle = "Made by Chung credit #Chungdz",
-       Note = "this guy is gay gbaox_01",
-       FileName = "0", 
-       SaveKey = true,
-       GrabKeyFromSite = true,
-       Key = {"https://raw.githubusercontent.com/Chungdz09/Script-chung-dz/refs/heads/main/Key"}
-    }
- })
-
- local MainTab = Window:CreateTab("🏠 Main", nil) 
- local MainSection = MainTab:CreateSection("Other Scripts")
- 
 local AutoAttackPlayer = false
 local AttackPlayerLoop
 local TargetPlayerName = ""
